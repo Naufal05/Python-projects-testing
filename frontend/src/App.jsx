2e38b8b4eaee889514c7da7b7c25ea2987cd0e89
NaufalMZ05 <mohamed@abia.com.au>
2026-05-18 13:55:42 +0530

    frontend-setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-indigo-600">
+      <h1 className="text-4xl font-extrabold text-white shadow-md p-6 bg-indigo-700 rounded-xl">
+        Tailwind is working perfectly on Localhost!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
